c0f61b65165bcc35aaa2b9a47aa80bcfd542a1fc
ajay2728 <40559182+ajay2728@users.noreply.github.com>
2018-10-10 19:38:05 +0530

    Update foundation.js

--- foundation.js
+++ foundation.js
@@ -3,6 +3,7 @@ var a = new Date();
 var b = a.getHours();
 var c = a.getMinutes();
 var d = a.getSeconds();
+var e = a.getMillieconds();
 if (b < 10){
   b = "0" + b;
 }
@@ -12,12 +13,16 @@ if (c < 10) {
 if (d < 10){
   d = "0" + d;
 }
+if(e < 10){
+  e = "0" + e;
+}
 
 document.getElementById("hours").innerHTML =  b;
 document.getElementById("minutes").innerHTML =  ": " + c;
 document.getElementById("seconds").innerHTML =  ": " + d;
+document.getElementById("milliSeconds").innerHTML = ": " + e;
 }
-setInterval(clock , 1000);
+setInterval(clock , 1);
 function signup(){
 document.getElementById("signup").style.display = "block"
 }
